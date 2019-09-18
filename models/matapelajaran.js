16bbce75fc613ac9df241f1698006511c6b389e5
Elfin Sanjaya <elfinsanjaya1207@gmail.com>
2019-09-18 10:25:25 +0700

    03. Mata Pelajaran (Migration)

--- models/matapelajaran.js
+++ models/matapelajaran.js
@@ -3,10 +3,10 @@ module.exports = (sequelize, DataTypes) => {
   const MataPelajaran = sequelize.define('MataPelajaran', {
     kode: DataTypes.STRING,
     nama: DataTypes.STRING,
-    nilai - kkm: DataTypes.INTEGER,
+    nilaikkm: DataTypes.INTEGER,
     TahunId: DataTypes.INTEGER
   }, {});
-  MataPelajaran.associate = function(models) {
+  MataPelajaran.associate = function (models) {
     // associations can be defined here
   };
   return MataPelajaran;
