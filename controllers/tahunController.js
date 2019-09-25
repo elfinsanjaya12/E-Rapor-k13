3a632b4a29b146c52c518cd55e8ee63498cf3353
Elfin Sanjaya <elfinsanjaya1207@gmail.com>
2019-09-25 23:33:31 +0700

    06. Tahun (Create)

--- controllers/tahunController.js
+++ controllers/tahunController.js
@@ -5,7 +5,11 @@ const Op = require("sequelize").Op;
 exports.viewTahun = async (req, res) => {
   try {
     const userLogin = req.session.user
-    const tahun = await Tahun.findAll()
+    const tahun = await Tahun.findAll({
+      order: [
+        ['id', 'DESC'],
+      ],
+    })
 
     res.render('admin/tahun/view_tahun', {
       user: userLogin,
@@ -14,4 +18,21 @@ exports.viewTahun = async (req, res) => {
   } catch (err) {
     throw err
   }
+}
+
+
+exports.actionCreate = async (req, res) => {
+  const { kepala_sekolah, nip, semester, tgl_raport, tahun } = req.body
+
+  await Tahun.create({
+    tahun: tahun,
+    kepala_sekolah: kepala_sekolah,
+    nip: nip,
+    semester: semester,
+    tgl_raport: tgl_raport,
+    status: "Nonactive"
+  })
+
+  res.redirect("/admin/tahun");
+
 }
--- routes/tahun.js
+++ routes/tahun.js
@@ -2,11 +2,13 @@ var express = require('express');
 var router = express.Router();
 let {
   viewTahun,
+  actionCreate
 } = require("../controllers/tahunController")
 
 const auth = require('../middlewares/auth')
 
 
 router.get("/admin/tahun", auth.isLogin, viewTahun)
+router.post("/admin/tahun/create", actionCreate)
 
 module.exports = router;
