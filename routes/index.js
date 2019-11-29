770c5bb5fb2cb63095834e64b58e227789c84f0b
Elfin Sanjaya <elfinsanjaya1207@gmail.com>
2019-11-29 22:49:45 +0700

    dashboard

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function (req, res, next) {
+const auth = require('../middlewares/auth')
+
+router.get('/', auth.isLogin, function (req, res, next) {
   res.redirect("/admin")
 });
 
 /* GET home page. */
-router.get('/admin', function (req, res, next) {
-  res.render('admin/dashboard/dashboard');
+router.get('/admin', auth.isLogin, function (req, res, next) {
+  const userLogin = req.session.user
+  console.log(userLogin)
+  res.render('admin/dashboard/dashboard', {
+    title: "Dashboard",
+    user: userLogin
+  });
 });
 
 module.exports = router;
