ea932d38a1a7bf7eb8f22e6d8810f1f094780522
Elfin Sanjaya <elfinsanjaya1207@gmail.com>
2019-11-30 05:39:28 +0700

    update notif middlewares

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,7 @@
 const isLogin = (req, res, next) => {
   if (req.session.user == null || req.session.user == undefined) {
+    req.flash('alertMessage', 'Mohon Maaf Session Anda Telah Habis Silahakan Login Kembali!');
+    req.flash('alertStatus', 'danger');
     res.redirect('/signin');
   } else {
     next();
