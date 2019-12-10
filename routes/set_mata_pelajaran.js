9292e099fda71d018235105596b45ab9e208d160
Elfin Sanjaya <elfinsanjaya1207@gmail.com>
2019-12-10 10:43:46 +0700

    set matpel guru add & delete

--- routes/set_mata_pelajaran.js
+++ routes/set_mata_pelajaran.js
@@ -2,11 +2,15 @@ var express = require('express');
 var router = express.Router();
 let {
   viewSetMatpel,
+  actionCreate,
+  actionDelete
 } = require("../controllers/setMatpelController")
 
 const auth = require('../middlewares/auth')
 
-router.get("/admin/set-matpel", auth.isLogin, viewSetMatpel)
-
+router.use(auth.isLogin)
+router.get("/admin/set-matpel", viewSetMatpel)
+router.post("/admin/set-matpel", actionCreate)
+router.get("/admin/set-matpel/:id", actionDelete)
 
 module.exports = router;
